ecb4ecf5ba4fb252059ee4048ebdbbe1463b071c
Narayanan0303 <balanarayanan3399@gmail.com>
2024-07-20 12:12:29 +0530

    Appointment start visit Webelement added

--- src/main/java/com/OpenMRS_JulyIPT/pom/AppointmentBookingPage.java
+++ src/main/java/com/OpenMRS_JulyIPT/pom/AppointmentBookingPage.java
@@ -1,5 +1,10 @@
 package com.OpenMRS_JulyIPT.pom;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class AppointmentBookingPage {
+	@FindBy(xpath = "//div[contains(text(),'Start Visit')]")
+	private WebElement startvisit;
 
 }
