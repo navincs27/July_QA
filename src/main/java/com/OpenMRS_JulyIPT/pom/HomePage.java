1fe2b9c463b3927824e6e05c9662a4d55fe4b3ff
ManjuNataraj <manjurajan59@gmail.com>
2024-07-20 12:11:23 +0530

    Homepage username webelement added

--- src/main/java/com/OpenMRS_JulyIPT/pom/HomePage.java
+++ src/main/java/com/OpenMRS_JulyIPT/pom/HomePage.java
@@ -1,5 +1,30 @@
 package com.OpenMRS_JulyIPT.pom;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class HomePage {
+	
+	private WebDriver logindriver;
+	
+	public HomePage( WebDriver Consdriver) {
+		
+		logindriver = Consdriver;
+		PageFactory.initElements(logindriver, this);
+	}
+	
+	@FindBy(id = "username")
+	private WebElement userName;
+	
+	@FindBy(id = "password")
+	private WebElement passWord;
+	
+	@FindBy(id = "Inpatient Ward")
+	private WebElement location;
+	
+	@FindBy(id = "loginButton" )
+	private WebElement loginBtn;
 
 }
